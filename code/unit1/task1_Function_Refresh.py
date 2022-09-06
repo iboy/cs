bd57f12dac73d6e0624701f91857db11976443b1
Ian Grant <ian.grant@mac.com>
2022-09-06 10:52:57 +0100

    Create task1_Function_Refresh.py

---
+++ code/unit1/task1_Function_Refresh.py
@@ -0,0 +1,18 @@
+# Unit 1 4.1.2.2 #
+# Task 1 - Function Refresh
+
+def hello():
+  print("Hello from a function")
+
+
+def my_function():
+  print("Hello from a function")
+my_function()
+
+
+def isCool(fname):
+  print(fname + " is cool!")
+
+isCool("Emil")
+isCool("Tobias")
+isCool("Linus")
