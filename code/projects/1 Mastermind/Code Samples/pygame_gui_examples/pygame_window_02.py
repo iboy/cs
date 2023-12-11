2db2a241943e8a9323a50bd5a194ac0f95b91075
Ian Grant <ian.grant@mac.com>
2023-12-11 11:12:25 +0000

    Pygame - some GUI code examples

---
+++ code/projects/1 Mastermind/Code Samples/pygame_gui_examples/pygame_window_02.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+pygame.init()
+
+pygame.display.set_caption('Quick Start')
+window_surface = pygame.display.set_mode((800, 600))
+
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color('#ffffff'))
+
+is_running = True
+
+while is_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+    window_surface.blit(background, (0, 0))
+    pygame.display.update()               
